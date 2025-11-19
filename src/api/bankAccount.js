3de6810df3d2e5ebf829c8417778a4b977bf1b9f
Mateis <mateisbourlet9@gmail.com>
2025-11-19 14:12:31 +0100

    merge success profile / dashboard

--- src/api/bankAccount.js
+++ src/api/bankAccount.js
@@ -1,15 +1,40 @@
-export const getAllBankAccounts = async (uid) => {
+export const createBankAccount = async  (
+    account
+) => {
     try {
-        const res = await fetch("http://localhost:8000/bank_account/all-bank-accounts/" + uid, {
+        const res = await fetch("http://localhost:8000/bank_account", {
+            method: "POST",
             headers: {
+                "Content-Type": "application/json",
                 authorization: `Bearer ${sessionStorage.getItem("access_token")}`
-            }
-        });
-
-        if(res.ok){
-            return res.json();
-        }
-    } catch (e) {
-        console.error(e);
+            },
+            body: JSON.stringify(account)
+        })
+        return await res.json();
+    } catch (error) {
+        console.log(error)
     }
 }
+
+
+export const deleteBankAccount = async (iban) => {
+  try {
+    const res = await fetch(`http://localhost:8000/bank_account/close/${iban}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+      }
+    });
+
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.detail || "Erreur de supression ");
+    }
+
+    return data;
+  } catch (error) {
+    console.log("banque delete error:", error);
+    return { error: error.message };
+  }
+};
